5a6daf8d4bbf6d30361b47f35104e8125685dab7
Consisten14 <905632925@qq.com>
2019-02-24 23:19:59 +0800

    !

---
+++ Unity3dMvvm/Assets/Scripts/Extension/SqlServeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace Assets.Scripts.Extension
+{
+    public static class SqlServeHelper
+    {
+        public static SqlConnection GetSqlConnection(string source, string catalog)
+        {
+            var show = string.Format("Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID=sa;Password=admin", source, catalog);
+
+            return new SqlConnection(show);
+        }
+       
+    }
+}
---
+++ Unity3dMvvm/Assets/Scripts/MVVM/LoginView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using uMVVM.Sources.Infrastructure;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.MVVM
+{
+    public class LoginView : UnityGuiView<LoginViewModel>
+    {
+        public InputField AccountInput;
+
+        public InputField PasswordInput;
+
+        public Text AccountText;
+
+        public Text PasswordText;
+
+        public Image HintPanel;
+
+        public Text HintText;
+
+        public Button LoginBtn;
+
+        public Button RegisterBtn;
+
+    }
+}
