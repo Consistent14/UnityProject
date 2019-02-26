1de34954261891aa93b5b075c96fc9f821fdae8a
Consisten14 <905632925@qq.com>
2019-02-27 07:41:00 +0800

    !

---
+++ UnityConsole/SqlServerHelper/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace SqlServerHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "server = localhost;database = abc;user =sa;password =admin";
+
+            var conn = new SqlConnection(host);
+
+            conn.Open();
+
+            var cmd = new SqlCommand("select * from [login] where account = '93'", conn);
+
+            var rd = cmd.ExecuteReader();
+
+            rd.Read();
+
+            var dk = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            var dp = Path.Combine(dk, "js.json");
+
+
+            if (rd.HasRows)
+            {
+                {
+               
+                        File.WriteAllText(dp, "have");
+                }
+            }
+            else
+            {
+                File.WriteAllText(dp, "nothing");
+
+            }
+        }
+    }
+}
